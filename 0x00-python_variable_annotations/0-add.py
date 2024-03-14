2745dec623cc6a056d7a01da6ad885265048381b
Ahmed Olalekan <olalekanahmed6@gmail.com>
2024-03-14 22:08:29 +0100

    add setups and solutions

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+Implementation of Static typing of Python
+"""
+
+
+def add(a: float, b: float) -> float:
+    """ Returns the addition two number
+    """
+    return a + b
