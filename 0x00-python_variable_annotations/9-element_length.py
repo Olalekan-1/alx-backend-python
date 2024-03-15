5415857ab1e98d8ba4055c4ba9fa2cc0be910716
Ahmed Olalekan <olalekanahmed6@gmail.com>
2024-03-15 02:04:44 +0100

    add solution

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -3,11 +3,11 @@
 """
 Implementation of Static typing of Python
 """
-
 from typing import Sequence, Tuple
 
 
 def element_length(lst: Sequence[str]) -> Sequence[Tuple[str, int]]:
-    """Returns a sequence of tuples containing each element and its length.
+    """
+    Returns a sequence of tuples containing each element and its length.
     """
     return [(i, len(i)) for i in lst]
