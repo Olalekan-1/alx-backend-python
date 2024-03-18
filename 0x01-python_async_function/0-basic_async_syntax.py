c0ca1260e746fec805eb7f52c4122892f34df375
Ahmed Olalekan <olalekanahmed6@gmail.com>
2024-03-18 16:18:35 +0100

    add solution

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
-"""implement an async function"""
 
+"""
+Implementation of python Async function - Co-routine
+"""
 import asyncio
-from random import uniform
+import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """wait for a random amunt of time and return time"""
-    time: float = uniform(0, max_delay)
+    """
+    Waits for an random time between the range of 0 to max_delay
+    Return:
+    The random time generated
+    """
+    time: int = random.uniform(0, max_delay)
     await asyncio.sleep(time)
     return time
