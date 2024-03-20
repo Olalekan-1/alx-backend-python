78cb82f2d9152474492092a3011ec717e8ce3147
Ahmed Olalekan <olalekanahmed6@gmail.com>
2024-03-20 05:55:00 +0100

    add

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """
+Task 0 - Write a co-routine function called async_generator
 Implementation of python Async function - Co-routine
 """
 import asyncio
@@ -8,7 +9,9 @@ from typing import AsyncGenerator
 
 
 async def async_generator() -> AsyncGenerator[float, None]:
-    """ Async generator """
+    """ Async generator 
+    Generates 10 numbers at regular interval
+    """
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
